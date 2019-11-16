b1f8a8623f3df02893a79b03d0a142e7f236c384
wwyws0000 <38261044+wwyws0000@users.noreply.github.com>
2019-11-15 20:55:17 -0500

    ALS Update

--- doc/ww_test.R
+++ doc/ww_test.R
@@ -17,9 +17,7 @@ source("../lib/cross_validation.R")
 
 data <- read.csv("../data/ml-latest-small/ratings.csv")
 
-
 set.seed(0)
-maxIter <- 10
 
 
 dataSplit <- train_test_split(data,0.8) #this function ensures that all users and movies are in the training set. 
@@ -27,7 +25,7 @@ data_train <- dataSplit$train
 data_test <- dataSplit$test
 
 
-registerDoParallel(cores = detectCores())
+registerDoParallel(cores = detectCores())# use parallelization for faster procesing
 
 out <- als.t(f = 10,  lambda = 0.1,max.iter = 1,data = data, train = data_train, test = data_test)
 
