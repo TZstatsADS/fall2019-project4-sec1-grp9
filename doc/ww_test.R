218258f268c94e6c76a5eebf57fdf10df3dce676
wwyws0000 <38261044+wwyws0000@users.noreply.github.com>
2019-11-19 08:26:00 -0500

    ALS Result

--- doc/ww_test.R
+++ doc/ww_test.R
@@ -27,4 +27,38 @@ data_test <- dataSplit$test
 
 registerDoParallel(cores = detectCores())# use parallelization for faster procesing
 
-out <- als.t(f = 10,  lambda = 0.01,max.iter = 10,data = data, train = data_train, test = data_test)
+# out <- als.t(f = 10,  lambda = 0.01,max.iter = 10,data = data, train = data_train, test = data_test)
+
+
+
+f_list <- c(10,20)
+l_list <- c(0.01,0.05,0.1)
+f_l <- expand.grid(f_list, l_list)
+
+
+
+# resultList <- list()
+# run_time <- system.time(for(i in 1:nrow(f_l)){
+#   par <- paste("f = ", f_l[i,1], ", lambda = ", f_l[i,2])
+#   cat(par, "\n")
+# 
+#   current_result  <- cv.als.t.function(data, K = 5, f = f_l[i,1], lambda = f_l[i,2], max.iter = 10)
+#   resultList[[paste0('f = ',f_l[i,1],' , lambda = ',f_l[i,2])]] <- current_result
+# 
+# 
+# })
+
+resultList <- list()
+run_time <- system.time(for(i in 1:nrow(f_l)){
+  par <- paste("f = ", f_l[i,1], ", lambda = ", f_l[i,2])
+  cat(par, "\n")
+  
+  current_result  <- als.t(f = f_l[i,1],  lambda = f_l[i,2],max.iter = 10,data = data, train = data_train, test = data_test)
+  resultList[[paste0('f = ',f_l[i,1],' , lambda = ',f_l[i,2])]] <- current_result
+  
+  
+})
+
+save(resultList, file = "../output/CVResult.RData")
+
+
